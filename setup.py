6c3ea103b0ec88c6d92603c07d3ad89e1cb9df60
John <john.cormican@mpinat.mpg.de>
2023-12-13 14:14:57 +0100

    inSPIRE-interactive-1.0rc0 packaging templates

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='inspire-interact',
@@ -6,9 +6,6 @@ setup(
     description='Interactive GUI for inSPIRE Platform.',
     author='John Cormican, Sahil Khan, Juliane Liepe, Manuel S. Pereira',
     author_email='juliane.liepe@mpinat.mpg.de',
-    packages=[
-        'inspire_interact',
-    ],
     include_package_data=True,
     long_description=open('README.md', mode='r', encoding='UTF-8').read(),
     long_description_content_type='text/markdown',
@@ -20,6 +17,7 @@ setup(
             'inspire-interact=inspire_interact.api:main',
         ]
     },
+    packages=find_packages(),
     python_requires='>=3.11',
     install_requires=[
         # 'inspirems==2.0rc01',
@@ -29,10 +27,11 @@ setup(
         'flask_cors==3.0.10',
         'itsdangerous==2.1.2',
         'psutil==5.9.6',
-        'Werkzeug==2.3.4',
+        'Werkzeug==3.0.1',
     ],
     project_urls={
         'Homepage': 'https://github.com/QuantSysBio/inSPIRE',
         'Tracker': 'https://github.com/QuantSysBio/inSPIRE/issues',
     },
+    zip_safe=False,
 )
