38258833b6b3386124fee3fde7cacaf82e891415
Cormican <john.cormican@s1604-mascot01.mpibpc.intern>
2024-03-18 16:18:37 +0100

    inSPIRE-interactive updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inspire-interact',
-    version='0.4',
+    version='0.5',
     description='Interactive GUI for inSPIRE Platform.',
     author='John Cormican, Sahil Khan, Juliane Liepe, Manuel S. Pereira',
     author_email='juliane.liepe@mpinat.mpg.de',
@@ -20,7 +20,7 @@ setup(
     packages=find_packages(),
     python_requires='>=3.11',
     install_requires=[
-        'inspirems==2.0rc5',
+        'inspirems==2.0rc6',
         'blinker==1.6.2',
         'click==8.1.3',
         'flask==2.3.2',
